9e06c733dd32fb8e716e7794f5a33487eee5132f
Yichieh0 <102524142+Yichieh0@users.noreply.github.com>
2022-08-17 15:17:23 +0800

    Update main.cpp

--- src/QAM/main.cpp
+++ src/QAM/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "QAM.hpp"
+#include "qam.hpp"
 #include "deQAM.hpp"
 #include "data_path.hpp"
 #include <math.h>
@@ -17,34 +17,22 @@ int main(int argc, char *argv[]){
 	hls::stream<int> para_str_in;
 	hls::stream<int> para_str_temp;
 	hls::stream<int> para_str_out;
+
+	int para_num = 5;
 	int qam_num =  16;
 	int sym_num =  2;
-	int DATA_LEN =  48;
+	int DATA_LEN =  30;
 	int pilot_width =  4;
 	int CP_length = 16;
 
-	cout << ">> Start test!" << endl;
+	para_str_in.write(para_num);
+	para_str_in.write(DATA_LEN);
+	para_str_in.write(qam_num);
+	para_str_in.write(sym_num);
+	para_str_in.write(pilot_width);
+	para_str_in.write(CP_length);
 
-	for(int k = 0; k < para_num; k++){
-		if(k==0){
-			para_str_in.write(DATA_LEN);
-		}
-		else if(k==1){
-			para_str_in.write(qam_num);
-		}
-		else if(k==2){
-			para_str_in.write(sym_num);
-		}
-		else if(k==3){
-			para_str_in.write(pilot_width);
-		}
-		else if(k==4){
-			para_str_in.write(CP_length);
-		}
-		else{
-			break;
-		}
-	}
+	cout << ">> Start test!" << endl;
 	for(int k = 0; k < (DATA_LEN*sym_num); k++){
 		QAM_in.write((ap_uint<QAM_unsigned_bit>)k);
 	}
