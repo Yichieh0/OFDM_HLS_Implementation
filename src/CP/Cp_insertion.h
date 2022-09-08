ed00e81a7369ca1b19e27ede54308f96e6624998
Yichieh0 <102524142+Yichieh0@users.noreply.github.com>
2022-09-09 00:01:29 +0800

    Delete src directory

--- src/CP/Cp_insertion.h
+++
@@ -1,8 +0,0 @@
-#ifndef __CP_INSERTION_H__
-#define __CP_INSERTION_H__
-
-#include "data_path.hpp"
-
-void Cp_insertion(hls::stream<ap_fixed<IN_WL, IN_IL>>& data_in_real, hls::stream<ap_fixed<IN_WL, IN_IL>>& data_in_imag, hls::stream<ap_fixed<IN_WL, IN_IL>>& data_out_real, hls::stream<ap_fixed<IN_WL, IN_IL>>& data_out_imag, hls::stream<int>& para_str_in, hls::stream<int>& para_str_out);
-
-#endif
