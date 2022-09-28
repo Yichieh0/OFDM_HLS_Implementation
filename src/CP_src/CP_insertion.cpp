68ece5bab3b7ddeebc44eba0ed394b5336c7fe43
Yichieh0 <102524142+Yichieh0@users.noreply.github.com>
2022-09-28 16:37:39 +0800

    Update CP_insertion.cpp

--- src/CP_src/CP_insertion.cpp
+++ src/CP_src/CP_insertion.cpp
@@ -3,13 +3,14 @@
 
 void CP_insertion(hls::stream<ap_axiu_64>& data_in, hls::stream<ap_axiu_64>& data_out)
 {
+#pragma HLS INTERFACE mode=ap_ctrl_none port=return
 #pragma HLS INTERFACE mode=axis register_mode=both port=data_out register
 #pragma HLS INTERFACE mode=axis register_mode=both port=data_in register
 
 	ap_uint_64 read_in;
 	ap_axiu_64 read_out;
-	ap_uint_32 out_temp_r;
-	ap_uint_32 out_temp_i;
+	ap_uint_32 out_temp_r = 0;
+	ap_uint_32 out_temp_i = 0;
 	ap_fixed<IN_WL, IN_IL> in_r[FFT_LEN];
 	ap_fixed<IN_WL, IN_IL> in_i[FFT_LEN];
 	
@@ -20,14 +21,17 @@ void CP_insertion(hls::stream<ap_axiu_64>& data_in, hls::stream<ap_axiu_64>& dat
 	int CP_length; 
 
 	ap_uint_64 para_in;
-	ap_axiu_64 para_out;
+	ap_axiu_64 para_axiu;
+	ap_axiu_64 data_axiu;
+	data_axiu.data = 0;
 	int module_id;
 	int para_id;
 	int para_val;
 	const int threshold = pow(2,module_id_bit)-1;
 
 	do{
-		para_in = data_in.read().data;
+		para_axiu = data_in.read();
+		para_in = para_axiu.data;
 		module_id = para_in.range(63, para_id_bit+para_val_bit);
 		if(module_id == module_num){
 			para_id = para_in.range(para_id_bit+para_val_bit-1, para_val_bit);
@@ -46,29 +50,30 @@ void CP_insertion(hls::stream<ap_axiu_64>& data_in, hls::stream<ap_axiu_64>& dat
 			}
 		}
 		else{
-			para_out.data = para_in;
-			data_out.write(para_out);
+			para_axiu.data = para_in;
+			data_out.write(para_axiu);
 		}
 	}while( module_id != threshold);
 
 	for(int k = 0; k<DATA_LEN*sym_num/(FFT_LEN-(FFT_LEN/pilot_width));k++){
 		for(int t = 0; t < (2*FFT_LEN); t++){
 			if(t < FFT_LEN){
-				read_in = data_in.read().data;
+				data_axiu = data_in.read();
+				read_in = data_axiu.data;
 				in_r[t].range(IN_WL-1,0) = read_in.range(32+IN_WL-1,32);
 				in_i[t].range(IN_WL-1,0) = read_in.range(IN_WL-1,0);
 			}
 			if((t >= FFT_LEN)){ //64
 				out_temp_r.range(IN_WL-1,0) = in_r[t-FFT_LEN].range(IN_WL-1,0);
 				out_temp_i.range(IN_WL-1,0) = in_i[t-FFT_LEN].range(IN_WL-1,0);
-				read_out.data = (out_temp_r,out_temp_i);
-				data_out.write(read_out);
+				data_axiu.data = (out_temp_r,out_temp_i);
+				data_out.write(data_axiu);
 			}
 			else if(t >= FFT_LEN-CP_length){ //48
 				out_temp_r.range(IN_WL-1,0) = in_r[t].range(IN_WL-1,0);
 				out_temp_i.range(IN_WL-1,0) = in_i[t].range(IN_WL-1,0);
-				read_out.data = (out_temp_r,out_temp_i);
-				data_out.write(read_out);
+				data_axiu.data = (out_temp_r,out_temp_i);
+				data_out.write(data_axiu);
 			}
 		}
 	}
