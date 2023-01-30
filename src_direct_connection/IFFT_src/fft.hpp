0743faf97a431a56d55a96226f5d740ef3e2194c
Yichieh0 <102524142+Yichieh0@users.noreply.github.com>
2023-01-30 17:07:34 +0800

    Update fft.hpp

--- src_direct_connection/IFFT_src/fft.hpp
+++ src_direct_connection/IFFT_src/fft.hpp
@@ -1,5 +1,5 @@
-#ifndef __FFT_H__
-#define __FFT_H__
+#ifndef __FFT_HPP__
+#define __FFT_HPP__
 #include "../data_path.hpp"
 
 void fft_top0(ap_uint_64_str& data_in, ap_uint_64_str& data_out); //IFFT
