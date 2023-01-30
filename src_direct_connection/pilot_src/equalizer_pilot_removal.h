d8da1b60cd009db40030cf965ba956e607b39411
Yichieh0 <102524142+Yichieh0@users.noreply.github.com>
2023-01-30 17:05:20 +0800

    Create equalizer_pilot_removal.h

---
+++ src_direct_connection/pilot_src/equalizer_pilot_removal.h
@@ -0,0 +1,8 @@
+#ifndef __EQUALIZER_PILOT_REMOVAL_H__
+#define __EQUALIZER_PILOT_REMOVAL_H__
+
+#include "../data_path.hpp"
+
+void equalizer_pilot_removal(ap_uint_64_str& data_in, ap_uint_64_str& data_out);
+
+#endif
