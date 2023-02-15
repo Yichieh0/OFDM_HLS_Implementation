ef733afd9a35ef0a18dc5de7c9a5face94728ff8
Yichieh0 <102524142+Yichieh0@users.noreply.github.com>
2023-02-15 11:54:41 +0800

    Delete direct_connection/vitis_hls_project directory

--- direct_connection/vitis_hls_project/settings.tcl
+++
@@ -1,8 +0,0 @@
-set XPART XCK26-SFVC784-2LV-C 
-set CSIM 0
-set CSYNTH 1
-set COSIM 1
-set VIVADO_SYN 0
-set VIVADO_IMPL 0
-set XF_PROJ_ROOT "/mnt/HLSNAS/g110064539/OFDM_Implemented_by_HLS/vitis_hls_project/Makefile"
-set CUR_DIR "/mnt/HLSNAS/g110064539/OFDM_Implemented_by_HLS/vitis_hls_project"
--- direct_connection/vitis_hls_project/src/pilot_src/pilot_insertion.h
+++
@@ -1,7 +0,0 @@
-#ifndef __PILOT_INSERTION_H__
-#define __PILOT_INSERTION_H__
-#include "../data_path.hpp"
-
-void pilot_insertion(ap_uint_64_str& data_in, ap_uint_64_str& data_out);
-
-#endif
