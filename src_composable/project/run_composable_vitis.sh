3c8aad7971cacede1b64ec57a94265196a1f514a
Yichieh0 <102524142+Yichieh0@users.noreply.github.com>
2023-02-24 03:41:28 +0800

    Update run_composable_vitis.sh

--- src_composable/project/run_composable_vitis.sh
+++ src_composable/project/run_composable_vitis.sh
@@ -3,30 +3,43 @@
 
 echo "start vitis_hls project"
 cd ./vitis_hls_composable_project/channel
+make clean
 make run CSIM=1 CSYNTH=1 COSIM=1 EXPORT_IP=1
 cd ../CP/CP_insertion
+make clean
 make run CSIM=1 CSYNTH=1 COSIM=1 EXPORT_IP=1
 cd ../CP_removal
+make clean
 make run CSIM=1 CSYNTH=1 COSIM=1 EXPORT_IP=1
 cd ../../ECC/decoder
+make clean
 make run CSIM=1 CSYNTH=1 COSIM=1 EXPORT_IP=1
 cd ../encoder
+make clean
 make run CSIM=1 CSYNTH=1 COSIM=1 EXPORT_IP=1
 cd ../../FFT/FFT
+make clean
 make run CSIM=1 CSYNTH=1 COSIM=1 EXPORT_IP=1
 cd ../IFFT
+make clean
 make run CSIM=1 CSYNTH=1 COSIM=1 EXPORT_IP=1
 cd ../../pilot/pilot_insertion
+make clean
 make run CSIM=1 CSYNTH=1 COSIM=1 EXPORT_IP=1
 cd ../pilot_removal
+make clean
 make run CSIM=1 CSYNTH=1 COSIM=1 EXPORT_IP=1
 cd ../../QAM/QAM
+make clean
 make run CSIM=1 CSYNTH=1 COSIM=1 EXPORT_IP=1
 cd ../deQAM
+make clean
 make run CSIM=1 CSYNTH=1 COSIM=1 EXPORT_IP=1
 cd ../../sym_pixl/pixl2sym
+make clean
 make run CSIM=1 CSYNTH=1 COSIM=1 EXPORT_IP=1
 cd ../sym2pixl
+make clean
 make run CSIM=1 CSYNTH=1 COSIM=1 EXPORT_IP=1
 
 echo "======================================================================"
