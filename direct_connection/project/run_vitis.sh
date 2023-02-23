283a773584457a39565c6492a633a51e58b6776d
Yichieh0 <102524142+Yichieh0@users.noreply.github.com>
2023-02-24 03:40:35 +0800

    Update run_vitis.sh

--- direct_connection/project/run_vitis.sh
+++ direct_connection/project/run_vitis.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
 # bash run_composable.sh
 
-echo "start vitis_hls project"
 cd ./vitis_hls_project
+make clean
+echo "start vitis_hls project"
 make run CSIM=1 CSYNTH=1 COSIM=1 EXPORT_IP=1
 
 echo "======================================================================"
