b145f4a0bb58c74ce223ddd4da165d3f90d3cab5
Yichieh0 <102524142+Yichieh0@users.noreply.github.com>
2023-02-23 23:57:52 +0800

    Update run_composable_main.sh

--- src_composable/project/run_composable_main.sh
+++ src_composable/project/run_composable_main.sh
@@ -2,3 +2,6 @@
 
 bash run_composable_vitis.sh
 bash run_composable_vivado.sh
+
+cp ./composable_communcation/composable_communcation.gen/sources_1/bd/commun/hw_handoff/commun.hwh commun.hwh
+cp ./composable_communcation/composable_communcation.runs/impl_1/commun_wrapper.bit commun.bit
