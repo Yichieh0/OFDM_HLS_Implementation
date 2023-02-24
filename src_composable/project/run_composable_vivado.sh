995c69c28d6933bbb046f8609f0ef8b58103594c
Yichieh0 <102524142+Yichieh0@users.noreply.github.com>
2023-02-25 01:33:45 +0800

    Update run_composable_vivado.sh

--- src_composable/project/run_composable_vivado.sh
+++ src_composable/project/run_composable_vivado.sh
@@ -19,6 +19,9 @@ fi
 
 vivado -source composable_communcation.tcl
 
+cp ./composable_communcation/composable_communcation.gen/sources_1/bd/commun/hw_handoff/commun.hwh commun.hwh
+cp ./composable_communcation/composable_communcation.runs/impl_1/commun_wrapper.bit commun.bit
+
 echo "======================================================================"
 echo "                           vivado complete "
 echo "======================================================================"
