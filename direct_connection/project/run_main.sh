e8e5dd017da04c8ccb94bb5b09222069300e3747
Yichieh0 <102524142+Yichieh0@users.noreply.github.com>
2023-02-25 01:32:52 +0800

    Update run_main.sh

--- direct_connection/project/run_main.sh
+++ direct_connection/project/run_main.sh
@@ -2,6 +2,3 @@
 
 bash run_vitis.sh
 bash run_vivado.sh
-
-cp ./vivado_project/vivado_project.gen/sources_1/bd/design_1/hw_handoff/design_1.hwh ./design_1.hwh
-cp ./vivado_project_save/vivado_project.runs/impl_1/design_1_wrapper.bit ./design_1.bit
