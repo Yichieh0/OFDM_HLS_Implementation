8db5f0e7becfacdedd371f5e2055e84d10922bfe
Yichieh0 <102524142+Yichieh0@users.noreply.github.com>
2023-02-26 17:16:31 +0800

    Update vivado_project.tcl

--- direct_connection/project/vivado_project.tcl
+++ direct_connection/project/vivado_project.tcl
@@ -1853,8 +1853,6 @@ move_dashboard_gadget -name {utilization_2} -row 1 -col 1
 move_dashboard_gadget -name {methodology_1} -row 2 -col 1
 
 update_compile_order -fileset sources_1
-#launch_runs synth_1
-#wait_on_run synth_1
 launch_runs impl_1 -to_step write_bitstream
 wait_on_run impl_1
 
